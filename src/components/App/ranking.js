b6e514efca0e1d3371c6c463b6f9a1c998189c79
Taru Airola <turger@wunder.dog>
2017-10-29 12:10:49 +0200

    Change ranking image after number three

--- src/components/App/ranking.js
+++ src/components/App/ranking.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Emoji from './emoji'
 import classNames from 'classnames'
 import Trophy from '../../assets/trophy.png'
+import Star from '../../assets/star.png'
 
 const Rank = ({ranking}) => (
   <div className="group__ranking">
-    <img src={ Trophy }/>
-    <div className={classNames('group__rankingnumber', { 'group__rankingnumber--big': ranking >= 10 })}>
+    <img src={ ranking < 4 ? Trophy : Star }/>
+    <div className={classNames('group__rankingnumber',
+      { 'group__rankingnumber--star': ranking >= 4 },
+      { 'group__rankingnumber--big': ranking >= 10 }
+    )}>
       { ranking }
     </div>
   </div>
