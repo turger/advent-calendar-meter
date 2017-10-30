294d6ce90de05816727ecd83965ca44be1a98f41
Taru Airola <turger@wunder.dog>
2017-10-30 17:29:32 +0200

    Add animation to trophys

--- src/components/App/ranking.js
+++ src/components/App/ranking.js
@@ -4,14 +4,13 @@ import Trophy from '../../assets/images/trophy.png'
 import Star from '../../assets/images/star.png'
 
 const Rank = ({ranking}) => (
-  <div className="group__ranking">
+  <div className={classNames('group__ranking',
+    { 'group__ranking--trophy': ranking < 4 },
+    { 'group__ranking--star': ranking >= 4 },
+    { 'group__ranking--star--bignum': ranking >= 10 }
+  )}>
     <img src={ ranking < 4 ? Trophy : Star }/>
-    <div className={classNames('group__rankingnumber',
-      { 'group__rankingnumber--star': ranking >= 4 },
-      { 'group__rankingnumber--big': ranking >= 10 }
-    )}>
-      { ranking }
-    </div>
+    <div className="group__rankingnumber"> { ranking } </div>
   </div>
 )
 
